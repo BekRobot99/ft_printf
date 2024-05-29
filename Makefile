c88a7d1747952d8280e0e00723bcf038d31d8e5a
Aymen Bekri <abekri@3-C-8.42heilbronn.de>
2024-05-29 18:04:54 +0200

    added

--- Makefile
+++ Makefile
@@ -1,41 +1,87 @@
-SRCS	=  ft_calc_len.c  ft_libft.c   ft_printf.c    \
-			ft_put_base.c ft_putptr.c  ft_type1.c ft_type2.c 
+# SRCS	=  ft_calc_len.c  ft_libft.c   ft_printf.c    \
+# 			ft_put_base.c ft_putptr.c  ft_type1.c ft_type2.c ft_memset.c minitalk_server_utils.c minitalk_client_utils.c 
 
-OBJS	= ${SRCS:.c=.o}
+# OBJS	= ${SRCS:.c=.o}
 
 
-NAME	= libftprintf.a
+# NAME	= libftprintf.a
 
-CC		= cc
+# CC		= cc
 
-RM		= rm -f
+# RM		= rm -f
 
-CFLAGS	= -Wall -Wextra -Werror
+# CFLAGS	= -Wall -Wextra -Werror
 
-.c.o:
-			${CC} ${CFLAGS} -c $< -o ${<:.c=.o}
+# .c.o:
+# 			${CC} ${CFLAGS} -c $< -o ${<:.c=.o}
 
+# %.o: %.c
+# 	$(CC) -c $(CFLAGS) -g $< -o $@
+
+# ${NAME}: ${OBJS}
+# 		ar -rcs ${NAME} ${OBJS}	 
+
+# # $(NAME): $(OBJS)
+# # 		${CC} ${CFLAGS} ${OBJS} -o ${NAME}
+
+# all:		${NAME}
+
+# clean:
+# # @$(MAKE) 
+# 	${RM} $(OBJS)
+
+# fclean: clean
+# # @$(MAKE)
+# 	${RM} $(NAME) $(OBJS)
+
+# re:			fclean all
+
+# 			ar -rcs ${NAME}  ${OBJS}
+
+# .PHONY:		all clean fclean re 
+
+
+# Source files
+SRCS = ft_calc_len.c ft_libft.c ft_printf.c \
+       ft_put_base.c ft_putptr.c ft_type1.c ft_type2.c \
+       ft_memset.c minitalk_server_utils.c minitalk_client_utils.c
+
+# Object files
+OBJS = $(SRCS:.c=.o)
+
+# Library name
+NAME = libftprintf.a
+
+# Compiler
+CC = cc
+
+# Compiler flags
+CFLAGS = -Wall -Wextra -Werror
+
+# Remove files command
+RM = rm -f
+
+# Default target
+all: $(NAME)
+
+# Rule to create object files from source files
 %.o: %.c
-	$(CC) -c $(CFLAGS) -g $< -o $@
+	$(CC) $(CFLAGS) -c $< -o $@
 
-${NAME}: ${OBJS}
-		ar -rcs ${NAME} ${OBJS}	 
+# Rule to create the library from object files
+$(NAME): $(OBJS)
+	ar -rcs $(NAME) $(OBJS)
 
-# $(NAME): $(OBJS)
-# 		${CC} ${CFLAGS} ${OBJS} -o ${NAME}
-
-all:		${NAME}
-
+# Clean object files
 clean:
-# @$(MAKE) 
-	${RM} $(OBJS)
+	$(RM) $(OBJS)
 
+# Clean object files and the library
 fclean: clean
-# @$(MAKE)
-	${RM} $(NAME) $(OBJS)
+	$(RM) $(NAME)
 
-re:			fclean all
+# Rebuild everything
+re: fclean all
 
-			ar -rcs ${NAME}  ${OBJS}
-
-.PHONY:		all clean fclean re 
+# Phony targets
+.PHONY: all clean fclean re
--- ft_printf.h
+++ ft_printf.h
@@ -5,16 +5,31 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: abekri <abekri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/02 02:24:13 by abekri            #+#    #+#             */
-/*   Updated: 2024/04/02 07:19:28 by abekri           ###   ########.fr       */
+/*   Created: 2024/05/29 17:33:35 by abekri            #+#    #+#             */
+/*   Updated: 2024/05/29 17:33:35 by abekri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
+# include <signal.h>
 # include <stdarg.h>
+# include <stdio.h>
 # include <unistd.h>
+# define STDERR stderr
+# define EXIT_FAILURE 0
 
+typedef struct t_handler
+{
+	int		pid;
+	int		i;
+	char	c;
+	char	*message;
+	int		msg_index;
+	int		buffer_size;
+}	t_handler_context_t;
+
+int		ft_atoi(const char *str);
 int		ft_printf(const char *format, ...);
 int		ft_put_in_format(char conv, va_list ap);
 int		ft_nbrlen(int n);
@@ -34,10 +49,22 @@ int		ft_put_hexa(unsigned int ap);
 int		ft_put_i(int ap);
 int		ft_put_u(unsigned int ap);
 int		ft_unbrlen(unsigned int n);
-
 void	ft_putchar_fd1(char c, int fd);
 void	ft_putnbr_fd1(int n, int fd);
 void	ft_putstr_fd1(char *s, int fd);
 char	*ft_strchr1(const char *str, int c);
 size_t	ft_strlen2(const char *s);
+void	*ft_memset(void *str, int c, size_t n);
+int		char_to_bin(char c, int pid);
+void	bin_to_char(int signum, char *c);
+void	check_argument_count(int argc, char *argv[]);
+int		get_pid(char *pid_arg);
+void	set_signal_handlers(struct sigaction *sa);
+void	send_message(char *message, int pid);
+char	*init_message_buffer(int buffer_size);
+void	reallocate_message_buffer(t_handler_context_t *ctx);
+void	process_received_char(t_handler_context_t *ctx);
+void	handle_end_of_message(t_handler_context_t *ctx);
+void	initialize_static_variables(t_handler_context_t *ctx);
+
 #endif
